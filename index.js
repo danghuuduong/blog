d51918e0a24ac6c14c2b509c4c85a02a587ecc35
Dang Huu Duong <danghuuduong98@gmail.com>
2023-08-14 15:57:00 +0700

    add lan 2

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
+app.use(morgan('combined'))
 app.get('/', function (req, res) {
-  res.send('Hello Worl212312323sd1')
+  res.send(
+    `
+      <h1> hello bn</h1>
+    `
+  )
 })
 
 app.listen(3000)
